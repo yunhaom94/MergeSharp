f8aeebe1d55e9b9cad5ba15afb0afdd967fa6c7c
yunhao <yunhaom94@outlook.com>
2022-10-13 01:54:48 +0000

    moved some files around

--- Examples/KVDB/Benchmark/benchmark.py
+++ Examples/KVDB/Benchmark/benchmark.py
@@ -8,7 +8,7 @@ import json
 import timeit
 import subprocess
 from enum import Enum
-from client import *
+from ../Cleint/client.py import *
 from draw import *
 import numpy as np
 from multiprocessing import Process, Pool
