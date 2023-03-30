446f0418b8cbaeccb7253ef8c020fe620e53dbf8
canlasla <lancecanlas25@gmail.com>
2023-03-30 15:33:38 -0400

    Add docstrings to PNCounter and TPTPGraph

--- MergeSharp/CRDTs/PNCounters.cs
+++ MergeSharp/CRDTs/PNCounters.cs
@@ -6,48 +6,73 @@ using System.Text.Json.Serialization;
 
 namespace MergeSharp;
 
+/// <summary>
+/// The <c>PropagationMessage</c> for the <c>PNCounter</c> Class.
+/// </summary>
 [TypeAntiEntropyProtocol(typeof(PNCounter))]
 public class PNCounterMsg : PropagationMessage
 {
+    /// <summary>
+    /// The class member for the positive values of the <c>PNCounterMsg</c>.
+    /// </summary>
     [JsonInclude]
     public Dictionary<Guid, int> pVector;
+
+    /// <summary>
+    /// The class member for the negative values of the <c>PNCounterMsg</c>.
+    /// </summary>
     [JsonInclude]
     public Dictionary<Guid, int> nVector;
-    
 
     public PNCounterMsg()
     {
     }
 
+    /// <summary>
+    /// Parametrized constructor for <c>PNCounterMsg</c>.
+    /// </summary>
+    /// <param name="pVector"><c>Dictionary</c> of {Guid, int} pairs containing the positive values.</param>
+    /// <param name="nVector"><c>Dictionary</c> of {Guid, int} pairs containing the negative values.</param>
     public PNCounterMsg(Dictionary<Guid, int> pVector, Dictionary<Guid, int> nVector)
     {
         this.pVector = pVector;
         this.nVector = nVector;
     }
 
-
+    /// <inheritdoc/>
     public override void Decode(byte[] input)
     {
         var json = JsonSerializer.Deserialize<PNCounterMsg>(input);
         this.pVector = json.pVector;
         this.nVector = json.nVector;
     }
-    
-    
+
+    /// <inheritdoc/>
     public override byte[] Encode()
     {
         return JsonSerializer.SerializeToUtf8Bytes(this);
     }
 }
 
-
+/// <summary>
+/// The <c>PNCounter</c> Class.
+/// </summary>
 [ReplicatedType("PNCounter")]
 public class PNCounter : CRDT
 {
-
+    /// <summary>
+    /// Member for positive values of the <c>PNCounter</c>.
+    /// </summary>
     private Dictionary<Guid, int> _pVector;
+
+    /// <summary>
+    /// Member for negative values of the <c>PNCounter</c>.
+    /// </summary>
     private Dictionary<Guid, int> _nVector;
 
+    /// <summary>
+    /// The class member for the replica ID of the <c>PNCounter</c>.
+    /// </summary>
     private Guid replicaIdx;
 
     public PNCounter()
@@ -60,12 +85,19 @@ public class PNCounter : CRDT
         this._nVector[this.replicaIdx] = 0;
     }
 
-
+    /// <summary>
+    /// Method to get the value of the <c>PNCounter</c>.
+    /// </summary>
+    /// <returns>Integer value of the <c>PNCounter</c>.</returns>
     public int Get()
     {
         return this._pVector.Sum(x => x.Value) - this._nVector.Sum(x => x.Value);
     }
 
+    /// <summary>
+    /// Method to increment the <c>PNCounter</c>.
+    /// </summary>
+    /// <param name="i">Value to increment the <c>PNCounter</c> by.</param>
     [OperationType(OpType.Update)]
     public virtual void Increment(int i)
     {
@@ -73,27 +105,34 @@ public class PNCounter : CRDT
         //this.HasSideEffect();
     }
 
+    /// <summary>
+    /// Method to decrement the <c>PNCounter</c>.
+    /// </summary>
+    /// <param name="i">Value to decrement the <c>PNCounter</c> by.</param>
     [OperationType(OpType.Update)]
     public virtual void Decrement(int i)
     {
         this._nVector[this.replicaIdx] += i;
         //this.HasSideEffect();
     }
-    
 
-
+    /// <inheritdoc/>
     public override PropagationMessage GetLastSynchronizedUpdate()
     {
         return new PNCounterMsg(this._pVector, this._nVector);
     }
 
+    /// <inheritdoc/>
     public override void ApplySynchronizedUpdate(PropagationMessage ReceivedUpdate)
     {
-        PNCounterMsg received = (PNCounterMsg)ReceivedUpdate;
+        PNCounterMsg received = (PNCounterMsg) ReceivedUpdate;
         this.Merge(received);
-
     }
 
+    /// <summary>
+    /// Method to merge a received update into this <c>PNCounter</c>.
+    /// </summary>
+    /// <param name="received">The update to be merged as a <c>PNCounterMsg</c>.</param>
     public void Merge(PNCounterMsg received)
     {
         foreach (var kv in received.pVector)
@@ -109,6 +148,7 @@ public class PNCounter : CRDT
         }
     }
 
+    /// <inheritdoc/>
     public override PropagationMessage DecodePropagationMessage(byte[] input)
     {
         PNCounterMsg msg = new();
--- MergeSharp/CRDTs/TPTPGraph.cs
+++ MergeSharp/CRDTs/TPTPGraph.cs
@@ -6,13 +6,21 @@ using System.Text.Json.Serialization;
 
 namespace MergeSharp;
 
+/// <summary>
+/// The <c>PropagationMessage</c> for <c>TPTPGraph</c> Class.
+/// </summary>
 [TypeAntiEntropyProtocol(typeof(TPTPGraph))]
-
 public class TPTPGraphMsg : PropagationMessage
 {
+    /// <summary>
+    /// The class member for the vertices of the <c>TPTPGraphMsg</c>.
+    /// </summary>
     [JsonInclude]
     public TPSetMsg<Guid> _verticesMsg;
 
+    /// <summary>
+    /// The class member for the edges of the <c>TPTPGraphMsg</c>.
+    /// </summary>
     [JsonInclude]
     public TPSetMsg<(Guid, Guid)> _edgesMsg;
 
@@ -20,12 +28,18 @@ public class TPTPGraphMsg : PropagationMessage
     {
     }
 
+    /// <summary>
+    /// Parametrized constructor for <c>TPTPGraphMsg</c>.
+    /// </summary>
+    /// <param name = "vertices">A <c>TPSet{Guid}</c> of vertices.</param>
+    /// <param name = "edges">A <c>TPSet{Guid, Guid}</c> of edges.</param>
     public TPTPGraphMsg(TPSet<Guid> vertices, TPSet<(Guid, Guid)> edges)
     {
         this._verticesMsg = (TPSetMsg<Guid>) vertices.GetLastSynchronizedUpdate();
         this._edgesMsg = (TPSetMsg<(Guid, Guid)>) edges.GetLastSynchronizedUpdate();
     }
 
+    /// <inheritdoc/>
     public override void Decode(byte[] input)
     {
         var json = JsonSerializer.Deserialize<TPTPGraphMsg>(input);
@@ -33,16 +47,27 @@ public class TPTPGraphMsg : PropagationMessage
         this._edgesMsg = json._edgesMsg;
     }
 
+    /// <inheritdoc/>
     public override byte[] Encode()
     {
         return JsonSerializer.SerializeToUtf8Bytes(this);
     }
 }
 
+/// <summary>
+/// The <c>TPTPGraph</c> Class.
+/// </summary>
 [ReplicatedType("TPTPGraph")]
 public class TPTPGraph : CRDT
 {
+    /// <summary>
+    /// Member for <c>TPTPGraph</c> vertices.
+    /// </summary>
     private readonly TPSet<Guid> _vertices;
+
+    /// <summary>
+    /// Member for <c>TPTPGraph</c> edges.
+    /// </summary>
     private readonly TPSet<(Guid, Guid)> _edges;
 
     public TPTPGraph()
@@ -51,12 +76,21 @@ public class TPTPGraph : CRDT
         this._edges = new TPSet<(Guid, Guid)>();
     }
 
+    /// <summary>
+    /// Method to add a vertex.
+    /// </summary>
+    /// <param name="v">Vertex to be added.</param>
     [OperationType(OpType.Update)]
     public virtual void AddVertex(Guid v)
     {
         this._vertices.Add(v);
     }
 
+    /// <summary>
+    /// Method to remove a vertex.
+    /// </summary>
+    /// <param name="v">Vertex to be removed.</param>
+    /// <returns> <c>True</c> if successfully added.</returns>
     [OperationType(OpType.Update)]
     public virtual bool RemoveVertex(Guid v)
     {
@@ -71,12 +105,19 @@ public class TPTPGraph : CRDT
         return false;
     }
 
+    /// <summary>
+    /// Method to add an edge.
+    /// </summary>
+    /// <param name="v1">First vertex of the edge.</param>
+    /// <param name="v2">Second vertex of the edge.</param>
+    /// <returns> <c>True</c> if successfully added.</returns>
     [OperationType(OpType.Update)]
     public virtual bool AddEdge(Guid v1, Guid v2)
     {
         var vertices = this.LookupVertices();
 
-        if (!vertices.Contains(v1) || !vertices.Contains(v2)) {
+        if (!vertices.Contains(v1) || !vertices.Contains(v2))
+        {
             return false;
         }
 
@@ -84,13 +125,22 @@ public class TPTPGraph : CRDT
         return true;
     }
 
+    /// <summary>
+    /// Method to remove an edge.
+    /// </summary>
+    /// <param name="v1">First vertex of the edge.</param>
+    /// <param name="v2">Second vertex of the edge.</param>
+    /// <returns> <c>True</c> if successfully added.</returns>
     [OperationType(OpType.Update)]
     public virtual bool RemoveEdge(Guid v1, Guid v2)
     {
         return this._edges.Remove((v1, v2));
     }
 
-
+    /// <summary>
+    /// Method that returns all edges in the <c>TPTPGraph</c>.
+    /// </summary>
+    /// <returns><c>IEnumerable{(Guid, Guid)}</c> of edges.</returns>
     public IEnumerable<(Guid, Guid)> LookupEdges()
     {
         List<(Guid, Guid)> edges = new();
@@ -108,24 +158,39 @@ public class TPTPGraph : CRDT
         return edges;
     }
 
+    /// <summary>
+    /// Method that returns all vertices in the <c>TPTPGraph</c>.
+    /// </summary>
+    /// <returns><c>IEnumerable{Guid}</c> of vertices.</returns>
     public IEnumerable<Guid> LookupVertices()
     {
         return this._vertices.LookupAll();
     }
 
+    /// <summary>
+    /// Method that returns if a vertex is in the <c>TPTPGraph</c>.
+    /// </summary>
+    /// <param name="v">Vertex to be checked.</param>
+    /// <returns><c>True</c> if the vertex is in the <c>TPTPGraph</c>.</returns>
     public bool Contains(Guid v)
     {
         return this.LookupVertices().Contains(v);
     }
 
+    /// <summary>
+    /// Method that returns if an edge is in the <c>TPTPGraph</c>.
+    /// </summary>
+    /// <param name="v1">First vertex of the edge.</param>
+    /// <param name="v2">Second vertex of the edge.</param>
+    /// <returns><c>True</c> if the edge is in the <c>TPTPGraph</c>.</returns>
     public bool Contains(Guid v1, Guid v2)
     {
         return this.LookupEdges().Contains((v1, v2));
     }
 
-
-
-    public override void ApplySynchronizedUpdate(PropagationMessage receivedUpdate){
+    /// <inheritdoc/>
+    public override void ApplySynchronizedUpdate(PropagationMessage receivedUpdate)
+    {
         if (receivedUpdate is not TPTPGraphMsg)
         {
             throw new NotSupportedException($"ApplySynchronizedUpdate does not support receivedUpdate type of {receivedUpdate.GetType()}");
@@ -136,13 +201,17 @@ public class TPTPGraph : CRDT
         this._vertices.ApplySynchronizedUpdate(received._verticesMsg);
     }
 
-    public override PropagationMessage DecodePropagationMessage(byte[] input) {
+    /// <inheritdoc/>
+    public override PropagationMessage DecodePropagationMessage(byte[] input)
+    {
         TPTPGraphMsg msg = new();
         msg.Decode(input);
         return msg;
     }
 
-    public override PropagationMessage GetLastSynchronizedUpdate() {
+    /// <inheritdoc/>
+    public override PropagationMessage GetLastSynchronizedUpdate()
+    {
         return new TPTPGraphMsg(this._vertices, this._edges);
     }
 }
