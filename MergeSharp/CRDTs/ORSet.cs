c8f66d7369939ee833db69d70494ece005052f58
Sean <seanpatrickllera@yahoo.ca>
2023-03-30 15:53:52 -0400

    Add docstrings for ORSet.cs

--- MergeSharp/CRDTs/ORSet.cs
+++ MergeSharp/CRDTs/ORSet.cs
@@ -7,17 +7,35 @@ using System.Text.Json.Serialization;
 
 namespace MergeSharp;
 
-
+/// <summary>
+/// Propagation Message for the <c>ORSetMsg{T}</c> class.
+/// </summary>
+/// <typeparam name="T">The type stored by the OR-Set</typeparam>
 [TypeAntiEntropyProtocol(typeof(ORSet<>))]
 public class ORSetMsg<T> : PropagationMessage
 {
+    /// <summary>
+    /// Represent's the <c>ORSetMsg{T}</c>'s add set.
+    /// </summary>
     [JsonInclude]
     public Dictionary<T, HashSet<Guid>> addSet { get; private set; }
+
+    /// <summary>
+    /// Represent's the <c>ORSetMsg{T}</c>'s remove set.
+    /// </summary>
+    /// <value></value>
     [JsonInclude]
     public Dictionary<T, HashSet<Guid>> removeSet { get; private set; }
 
+    /// <summary>
+    /// Represent's the <c>ORSetMsg{T}</c>'s added null values set.
+    /// </summary>
     [JsonInclude]
     public HashSet<Guid> nullAddGuid { get; private set; }
+
+    /// <summary>
+    /// Represent's the <c>ORSetMsg{T}</c>'s removed null values set.
+    /// </summary>
     [JsonInclude]
     public HashSet<Guid> nullRemoveGuid { get; private set; }
 
@@ -34,6 +52,7 @@ public class ORSetMsg<T> : PropagationMessage
         this.nullRemoveGuid = nullRemoveGuid;
     }
 
+    /// <inheritdoc />
     public override void Decode(byte[] input)
     {
         var json = JsonSerializer.Deserialize<ORSetMsg<T>>(input);
@@ -43,23 +62,48 @@ public class ORSetMsg<T> : PropagationMessage
         this.nullRemoveGuid = json.nullRemoveGuid;
     }
 
+    /// <inheritdoc />
     public override byte[] Encode()
     {
         return JsonSerializer.SerializeToUtf8Bytes(this);
     }
 }
 
+/// <summary>
+/// Class <c>ORSet{T}</c> models an Observed-Remove Set.
+/// </summary>
+/// <typeparam name="T"> The type stored by the OR-Set.</typeparam>
 [ReplicatedType("ORSet")]
 public class ORSet<T> : CRDT, ICollection<T>
 {
+    /// <summary>
+    /// Represent's the <c>ORSet{T}</c>'s add set.
+    /// </summary>
     private readonly Dictionary<T, HashSet<Guid>> addSet;
+
+    /// <summary>
+    /// Represent's the <c>ORSet{T}</c>'s remove set.
+    /// </summary>
     private readonly Dictionary<T, HashSet<Guid>> removeSet;
 
+    /// <summary>
+    /// Represent's the <c>ORSet{T}</c>'s added null values set.
+    /// </summary>
     private readonly HashSet<Guid> nullAddGuid;
+
+    /// <summary>
+    /// Represent's the <c>ORSet{T}</c>'s removed null values set.
+    /// </summary>
     private readonly HashSet<Guid> nullRemoveGuid;
 
+    /// <summary>
+    /// The number of elements in the <c>ORSet{T}</c>
+    /// </summary>
     public int Count => this.LookupAll().Count();
 
+    /// <summary>
+    /// Gets a value indicating whether the <c>ORSet{T}</c> is read-only.
+    /// </summary>
     public bool IsReadOnly => false;
 
     public ORSet()
@@ -70,12 +114,21 @@ public class ORSet<T> : CRDT, ICollection<T>
         this.nullRemoveGuid = new HashSet<Guid>();
     }
 
+    /// <summary>
+    /// Adds an item to the <c>ORSet{T}</c>.
+    /// </summary>
+    /// <param name="item">item to be added.</param>
     [OperationType(OpType.Update)]
     void ICollection<T>.Add(T item)
     {
         _ = this.Add(item);
     }
 
+    /// <summary>
+    /// Adds an item to the <c>ORSet{T}</c>.
+    /// </summary>
+    /// <param name="item">item to be added.</param>
+    /// <returns>Boolean indication whether the addition of the item was successful or not.</returns>
     [OperationType(OpType.Update)]
     public virtual bool Add(T item)
     {
@@ -98,6 +151,11 @@ public class ORSet<T> : CRDT, ICollection<T>
         return true;
     }
 
+    /// <summary>
+    /// Removes an item from the <c>ORSet{T}</c> if it exists in the set.
+    /// </summary>
+    /// <param name="item">item to be removed.</param>
+    /// <returns>Boolean indicating whether the removal of the item was successful or not.</returns>
     [OperationType(OpType.Update)]
     public virtual bool Remove(T item)
     {
@@ -126,6 +184,9 @@ public class ORSet<T> : CRDT, ICollection<T>
         }
     }
 
+    /// <summary>
+    /// Clears the <c>ORSet{T}</c>.
+    /// </summary>
     [OperationType(OpType.Update)]
     public virtual void Clear()
     {
@@ -135,12 +196,17 @@ public class ORSet<T> : CRDT, ICollection<T>
         this.nullRemoveGuid.Clear();
     }
 
-    // get the result set by looking at Guid's of removeSet and compare
-    // with addSet Guids. If a guid is in both sets, do not include
-    // the key value pair to the result set
-    // https://stackoverflow.com/questions/25735132/getkey-and-value-by-except-on-just-value
+    /// <summary>
+    /// Returns the elements of the OR-Set.
+    /// </summary>
+    /// <returns>An Enumerable of the elements in the OR-Set</returns>
     public IEnumerable<T> LookupAll()
     {
+        // get the result set by looking at Guid's of removeSet and compare
+        // with addSet Guids. If a guid is in both sets, do not include
+        // the key value pair to the result set
+        // https://stackoverflow.com/questions/25735132/getkey-and-value-by-except-on-just-value
+
         IEnumerable<T> inOnlyAdd = this.addSet.Keys.Except(this.removeSet.Keys);
 
         IEnumerable<T> inBothSets =
@@ -159,15 +225,30 @@ public class ORSet<T> : CRDT, ICollection<T>
         return inOnlyAdd.Union(inBothSets).Union(nullVal).ToList();
     }
 
+    /// <summary>
+    /// Determines if a given value is in the set.
+    /// </summary>
+    /// <param name="item">Item to find in the set.</param>
+    /// <returns>Boolean indicating if the given item is in the set.</returns>
     public bool Contains(T item)
     {
         return this.LookupAll().Contains(item);
     }
 
+    /// <summary>
+    /// Copies all the elements of the OR-Set to the specified one-dimensional array.
+    /// </summary>
+    /// <param name="array">one-dimensional array that the values wil be copied to.</param>
+    /// <param name="arrayIndex">integer that indicates the index in the array where copying begins.</param>
     public void CopyTo(T[] array, int arrayIndex)
     {
         this.LookupAll().ToArray().CopyTo(array, arrayIndex);
     }
+
+    /// <summary>
+    /// Merges the received OR-Set into this OR-Set.
+    /// </summary>
+    /// <param name="received">OR-Set that will be used for merging.</param>
     private void Merge(ORSetMsg<T> received)
     {
         foreach (KeyValuePair<T, HashSet<Guid>> r in received.addSet)
@@ -200,6 +281,7 @@ public class ORSet<T> : CRDT, ICollection<T>
         this.nullRemoveGuid.UnionWith(received.nullRemoveGuid);
     }
 
+    /// <inheritdoc />
     public override void ApplySynchronizedUpdate(PropagationMessage ReceivedUpdate)
     {
         if (ReceivedUpdate is not ORSetMsg<T>)
@@ -210,6 +292,7 @@ public class ORSet<T> : CRDT, ICollection<T>
         this.Merge(received);
     }
 
+    /// <inheritdoc />
     public override PropagationMessage DecodePropagationMessage(byte[] input)
     {
         ORSetMsg<T> msg = new();
@@ -217,17 +300,26 @@ public class ORSet<T> : CRDT, ICollection<T>
         return msg;
     }
 
+    /// <inheritdoc />
     public override PropagationMessage GetLastSynchronizedUpdate()
     {
         return new ORSetMsg<T>(this.addSet, this.removeSet, this.nullAddGuid, this.nullRemoveGuid);
     }
 
+    /// <summary>
+    /// Returns an enumerator that iterates through the collection.
+    /// </summary>
+    /// <returns>An enumerator that can be used to iterate through the collection.</returns>
 
     public IEnumerator<T> GetEnumerator()
     {
         return this.LookupAll().GetEnumerator();
     }
 
+    /// <summary>
+    /// Exposes an enumerator, which supports a simple iteration over a non-generic collection.
+    /// </summary>
+    /// <returns>An enumerator that can be used to iterate through the collection.</returns>
     IEnumerator IEnumerable.GetEnumerator()
     {
         return this.GetEnumerator();
